49fd9d64e8059475d2d2e792436c0cca945bd379
longzhao <292538769@qq.com>
2017-02-17 10:02:02 +0800

    broadCast--broadcast receiver

--- app/src/main/java/com/example/lahm/dailytask/LifeCircleActivity.java
+++ app/src/main/java/com/example/lahm/dailytask/LifeCircleActivity.java
@@ -1,5 +1,9 @@
 package com.example.lahm.dailytask;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +18,24 @@ import android.util.Log;
 
 public class LifeCircleActivity extends AppCompatActivity {
     private final String TAG = "lifeCircle";
+    private MyBroadcastReceiver myBroadcastReceiver;
+
+    class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String msg = intent.getStringExtra("msg");
+            Log.i(TAG, msg);
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate: ");
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.intent.action.MY_BROADCAST");
+        registerReceiver(myBroadcastReceiver, filter);
     }
 
 
@@ -26,6 +43,9 @@ public class LifeCircleActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart: ");
+        Intent intent = new Intent("android.intent.action.MY_BROADCAST");
+        intent.putExtra("msg", "hello receiver.");
+        sendBroadcast(intent);
     }
 
     @Override
@@ -62,6 +82,8 @@ public class LifeCircleActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy: ");
+        //动态注册广播，一定要反注册，否则报错--has leaked IntentReceiver
+        unregisterReceiver(myBroadcastReceiver);
     }
 
     @Override
@@ -75,5 +97,4 @@ public class LifeCircleActivity extends AppCompatActivity {
         super.onBackPressed();
         Log.i(TAG, "onBackPressed: ");
     }
-
 }
