46f9487964052e186ad803a039425beb7b61f2ee
longzhao <292538769@qq.com>
2017-03-30 15:28:16 +0800

    从Java Memory Mode角度分析Double Check Lock

--- app/src/main/java/com/example/lahm/dailytask/Singleton/LazySingle.java
+++ app/src/main/java/com/example/lahm/dailytask/Singleton/LazySingle.java
@@ -9,7 +9,16 @@ import android.util.Log;
  * https://github.com/lamster2018
  */
 public class LazySingle {
-    private static LazySingle lazySingle;
+    /**
+     * 因为对象的创建分三步，分配内存空间，初始化对象，将该对象内存空间地址赋给引用、
+     * 其中二三步可能发生重排序，则DCL的第二次判断会出错，
+     * lazySingle！=null（实际只是地址给了lazySingleton引用，而对象还未初始化）
+     * 声明lazySingle这个值（本质是个指针）为volatile，避免发生重排序
+     * 因为对volatile变量，也就是lazySingle进行读操作的时候，它后面的操作无法进行重排序
+     * happen-before原则规定
+     * http://cmsblogs.com/?p=2161
+     */
+    private volatile static LazySingle lazySingle;
 
     //thread unsafe mode
     public static LazySingle getLazySingle() {
--- lib/src/main/java/com/example/MultiThread/TestDeadLock.java
+++ lib/src/main/java/com/example/MultiThread/TestDeadLock.java
@@ -17,7 +17,22 @@ public class TestDeadLock {
 
         t1.start();
         t2.start();
-        while (true) ;
+
+        final String k = "BIG";
+//        k = k.toLowerCase();//编译错误
+        //因为String是不可变类，声明的一个指向String对象的指针k，
+        // k.toLow操作后会导致新的String产生，也就是k指向的对象的地址变化了
+        // final编译冲突了呗
+
+        final StringBuffer a = new StringBuffer("x");
+        final StringBuffer b = new StringBuffer("y");
+//        a=b;//编译错误
+        final StringBuffer sb = new StringBuffer("z");
+        sb.append("kkk");//sb指向的SB对象内容变化了
+//        可见，final只对引用的“值”(也即它所指向的那个对象的内存地址) 有效，
+//        它迫使引用只能指向初始指向的那个对象，改变它的指向会导致编译期错误。
+//        至于它所指向的对象的变化，final是不负责的。这很类似 == 操作符：==操作符只负责引用的“值”相等，
+//        至于这个地址所指向的对象内容是否相等，==操作符是不管的。
     }
 
 }
