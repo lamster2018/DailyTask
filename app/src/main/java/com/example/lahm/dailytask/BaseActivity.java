53e419d316af82b61c6839e39d608e36962e3baa
lahm <292538769@qq.com>
2017-08-17 17:29:11 +0800

    主界面绑定home和backpress

--- app/src/main/java/com/example/lahm/dailytask/BaseActivity.java
+++ app/src/main/java/com/example/lahm/dailytask/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.lahm.dailytask;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,18 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    //http://blog.163.com/shaocpa@126/blog/static/5535775720122534522863/
+    @Override
+    public void onBackPressed() {
+        if (this instanceof MainActivity) {
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addCategory(Intent.CATEGORY_HOME);
+            startActivity(i);
+        } else
+            super.onBackPressed();
     }
 
     @Override
