d2725beb96974d20657d9403eacc35c7df9fb47c
lahm <292538769@qq.com>
2017-08-24 17:23:01 +0800

    研究xml setBackground 和 textView setDrawable

--- app/src/main/java/com/example/lahm/dailytask/ImageActivity.java
+++ app/src/main/java/com/example/lahm/dailytask/ImageActivity.java
@@ -3,23 +3,32 @@ package com.example.lahm.dailytask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-
-import com.example.lahm.dailytask.Util.ImageUtil;
+import android.widget.TextView;
 
 public class ImageActivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
-        ImageView imageView = (ImageView) findViewById(R.id.iv);
+        textView = (TextView) findViewById(R.id.tv);
+        imageView = (ImageView) findViewById(R.id.iv);
+
 //        imageView.setImageBitmap();//实际调用setImageDrawable
 //        imageView.setImageDrawable();
 //        imageView.setImageResource(R.mipmap.giantimg);
-
-        imageView.setBackgroundResource(R.mipmap.giantimg);//实际调用setBackground
+//
+//        imageView.setBackgroundResource(R.mipmap.giantimg);//实际调用setBackground
 //        imageView.setBackground();//实际调用setBackgroundDrawable
-        imageView.setBackgroundDrawable(ImageUtil.setBackground(this, R.mipmap.giantimg));
+//        imageView.setBackgroundDrawable(ImageUtil.setBackground(this, R.mipmap.giantimg));
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textView.setCompoundDrawables(null, null, null, null);
     }
 }
